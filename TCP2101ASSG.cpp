93ca8b0d9c7babc8bbd039eb300d40cba4c9df91
Sarah Shahmina <sarahabdulrauf22@gmail.com>
2022-12-10 15:21:14 +0800

    Explanation of mr sharaf's code

--- TCP2101ASSG.cpp
+++ TCP2101ASSG.cpp
@@ -7,7 +7,12 @@
 // keys are unique, dsName are unique
 using namespace std;
 
-struct DeclObject
+struct DeleteObject
+{
+    string dsName;
+};
+
+struct DeclObject // A struct to declare the syntax of our DSL command
 {
     string varCommand;
     string dstype;
@@ -24,51 +29,51 @@ struct Variable
 
 class Program
 {
-    vector<string> commands;
+    vector<string> commands; //our DSL command
     // map<string, string> runningProgram;// string = DS name
     map<string, DeclObject> runningProgram; // string = DS name
 
 public:
     Program()
     {
-        string comm;
-        getline(cin, comm);
-        while (comm.compare("#") != 0)
-        { //# = eoline
-            commands.push_back(comm);
-            getline(cin, comm);
+        string comm; 
+        getline(cin, comm); //getting a command from the user
+        while (comm.compare("#") != 0) //# = eoline
+        {
+            commands.push_back(comm); //pushing back the user's inputted command to the command vectpr
+            getline(cin, comm); //A loop, asking user to input more until they input # (end of line)
         }
     }
 
     int countWords(string &str)
     {
-        stringstream s(str);
+        stringstream s(str); //breaks the user's command into individual words
         int counter = 0;
-        string wrd;
-        while (s >> wrd)
+        string wrd; // to store individual words
+        while (s >> wrd) //reads from the stringstream object
         {
-            counter++;
+            counter++; //for each word, increase the counter
         }
         return counter;
     }
 
     void parseCommands()
     {
-        DeclObject obj;
-        for (auto c : commands)
+        DeclObject obj;//declaring an object (so we can separate the users inputted commands)
+        for (auto c : commands)//for each of the user's commands
         {
-            stringstream s(c);
+            stringstream s(c); //allows you to read a string as if it were a stream (like cin)
 
-            int numOfWords = countWords(c);
-            if (numOfWords == 4)
+            int numOfWords = countWords(c); //count the number of words in a user's command
+            if (numOfWords == 4) //manages 4 word declarations
             {
-                s >> obj.varCommand;
+                s >> obj.varCommand; //stores the first word (command type) from the user's command to the object
                 if (obj.varCommand.compare("Var") == 0) // need to differentiate between the 2 vars
                 {
-                    s >> obj.dstype;
-                    s >> obj.itemtype;
-                    s >> obj.dsName;
-                    runningProgram[obj.dsName] = obj;
+                    s >> obj.dstype; //stores the second word (the data structure type) from user's command to the object
+                    s >> obj.itemtype; //stores the third word (the variable type) 
+                    s >> obj.dsName;  //stores the fourth word (name given to the data structure)
+                    runningProgram[obj.dsName] = obj; //add object to the runningProgram, key is dsName, therefore dsName should be unique
                     // cout << "item is added to the program (dynamic)" << endl;
                     // cout << runningProgram[obj.dsName].dsName << endl;
                 }
