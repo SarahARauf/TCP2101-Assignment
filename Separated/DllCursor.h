e6f6e1e3aa0e7d9beb0c15ee36e54bb9f005fc43
Sabrina Amalyn <88494163+SabrinaAmalyn@users.noreply.github.com>
2023-01-24 13:13:25 +0800

    VERSION 5

---
+++ Separated/DllCursor.h
@@ -0,0 +1,59 @@
+#ifndef DLLCURSOR_H
+#define DLLCURSOR_H
+
+class DeclObject;
+template <typename T>
+class Variable;
+template <typename T>
+class operations;
+template <typename T>
+class Datastruct;
+template <typename T>
+class DoubleLinkedList;
+class Program;
+
+
+#include <iostream>
+#include <map>
+#include <sstream>
+#include <vector>
+#include <iterator>
+#include <string>
+#include <algorithm>
+#include "DeclObject.h"
+#include "Variable.h"
+#include "operations.h"
+#include "Datastruct.h"
+#include "Dll.h"
+#include "Program.h"
+using namespace std;
+
+
+
+template <class T>
+class DLListCursor : public DeclObject<T>
+{
+    private:
+        DoubleLinkedList<T>* list;                      
+        typename DoubleLinkedList<T>::DNode* current;    
+
+    public:
+
+        DLListCursor();
+
+        void pointToStart(DoubleLinkedList<T>& dll);
+
+        void pointToEnd(DoubleLinkedList<T>& dll);
+
+        void moveToNext();
+
+        void moveToPrev();
+
+        T getNodeValue();
+
+        bool checkEnd();
+
+        bool checkStart();
+};
+
+#endif
